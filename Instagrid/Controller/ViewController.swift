17e672948d6731c9b8d07746f9c4d1cd55c2bcf1
Maxime <maxime.landuwa@gmail.com>
2021-08-19 12:05:35 +0200

    Portrait constraints OK, Images sizes to change, layout change OK

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,18 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var layout1: UIButton!
-    @IBOutlet weak var layout2: UIButton!
-    @IBOutlet weak var layout3: UIImageView!
+    @IBOutlet weak var layoutButton1: UIButton!
+    @IBOutlet weak var layoutButton2: UIButton!
+    @IBOutlet weak var layoutButton3: UIButton!
     
-    @IBOutlet weak var swipeUp: UILabel!
+    @IBOutlet weak var layoutView : layoutView!
+    
+    @IBOutlet weak var swipe: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(layoutSelect(_:)))
+        layoutView?.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func didTapLayoutButton1() {
+        layoutView.style = .layout1
+        print("Button 1")
+    }
+    @IBAction func didTapLayoutButton2() {
+        layoutView.style = .layout2
+        print("Button 2")
+    }
+    @IBAction func didTapLayoutButton3() {
+        layoutView.style = .layout3
+        print("Button 3")
     }
 
-
+    @IBAction func layoutSelect(_ sender: UIPanGestureRecognizer) {
+        
+    }
+    
+    @IBAction func swipeToShare(_ sender: UISwipeGestureRecognizer) {
+        
+    }
 }
-
-
---
+++ Instagrid/View/LayoutView.swift
@@ -0,0 +1,61 @@
+//
+//  LayoutView.swift
+//  Instagrid
+//
+//  Created by Maxime Landu-Wa on 18/08/2021.
+//
+
+import UIKit
+
+class layoutView: UIView {
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
+    
+    
+    @IBOutlet weak var selectedLayout1: UIImageView!
+    @IBOutlet weak var selectedLayout2: UIImageView!
+    @IBOutlet weak var selectedLayout3: UIImageView!
+    
+    enum Style {
+        case layout1, layout2, layout3
+    }
+    
+    var style: Style = .layout3{
+        //Appelle la fonction setStyle lorsque la propriété style est modifiée
+        didSet {
+            setStyle(style)
+        }
+    }
+    
+    private func setStyle (_ style: Style) {
+        switch style {
+        case.layout1:
+            image1.isHidden = false
+            image2.isHidden = true
+            image3.isHidden = false
+            image4.isHidden = false
+            selectedLayout1.isHidden = false
+            selectedLayout2.isHidden = true
+            selectedLayout3.isHidden = true
+        case.layout2:
+            image1.isHidden = false
+            image2.isHidden = false
+            image3.isHidden = false
+            image4.isHidden = true
+            selectedLayout1.isHidden = true
+            selectedLayout2.isHidden = false
+            selectedLayout3.isHidden = true
+        case.layout3:
+            image1.isHidden = false
+            image2.isHidden = false
+            image3.isHidden = false
+            image4.isHidden = false
+            selectedLayout1.isHidden = true
+            selectedLayout2.isHidden = true
+            selectedLayout3.isHidden = false
+        }
+    }
+}
