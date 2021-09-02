6984b3e5b92e3ffafe7eafc51e9dc1977f64ea84
Maxime <maxime.landuwa@gmail.com>
2021-09-02 20:11:19 +0200

    Comments done, still need to refactor swipeToShare IBAction and remove the test prints()

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,45 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
     //MARK: -Properties
+    
+    //Buttons used to choose the desired layout
     @IBOutlet weak var layoutButton1: UIButton!
     @IBOutlet weak var layoutButton2: UIButton!
     @IBOutlet weak var layoutButton3: UIButton!
     
+    //References LayoutView
     @IBOutlet weak var layoutView : LayoutView!
-
+    
     @IBOutlet weak var swipe: UIButton!
     
+    //Used to check which button has been pressed
     private var buttonSelect: Int = 0
     
     //MARK: -viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let imageTapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapLayoutImage1))
-        layoutView?.addGestureRecognizer(imageTapGesture)
-        
+    
     }
     
     //MARK: -Actions
+    
+    //Layout Button 1 action
     @IBAction func didTapLayoutButton1() {
         layoutView.style = .layout1
         print("Button 1")
     }
+    
+    //Layout Button 2 action
     @IBAction func didTapLayoutButton2() {
         layoutView.style = .layout2
         print("Button 2")
     }
+    
+    //Layout Button 3 action
     @IBAction func didTapLayoutButton3() {
         layoutView.style = .layout3
         print("Button 3")
     }
     
-    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
-        print("did tap image view", sender)
-    }
-    
-    @IBAction func didTapLayoutImage1(_ sender: UITapGestureRecognizer) {
+    //Opens the gallery to choose a picture to load into the selected slot
+    @IBAction func didTapLayoutImage(_ sender: UITapGestureRecognizer) {
         print("Button Pressed", sender)
+        //Changes the buttonSelect value to the tag of the view that sends the tap
         buttonSelect = sender.view?.tag ?? 1
         let vc = UIImagePickerController()
         vc.sourceType = .photoLibrary
@@ -55,7 +60,8 @@ class ViewController: UIViewController {
         vc.allowsEditing = true
         present(vc, animated: true)
     }
-
+    
+    //Enables user to swipe and share the final image, converts the view into an image
     @IBAction func swipeToShare(_ sender: UISwipeGestureRecognizer) {
         
         if UIDevice.current.orientation.isPortrait && sender.direction == .up{
@@ -85,25 +91,31 @@ class ViewController: UIViewController {
 }
 //MARK: -Extension
 extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //Applies the image to the view
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         print("\(info)")
         if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")]as? UIImage{
-            //layoutView.image1.image = image
             
-                        let selection = buttonSelect
-                        switch selection {
-                        case 1:
-                            layoutView.image1.image = image
-                        case 2:
-                            layoutView.image2.image = image
-                        case 3:
-                            layoutView.image3.image = image
-                        case 4:
-                            layoutView.image4.image = image
-                        default:
-                            break
-                        }
+            //Checks the tag of the selected view and applies the image to it, changes the aspect of the picture to fill the view
+            let selection = buttonSelect
+            switch selection {
+            case 1:
+                layoutView.image1.image = image
+                layoutView.image1.contentMode = .scaleAspectFill
+            case 2:
+                layoutView.image2.image = image
+                layoutView.image2.contentMode = .scaleAspectFill
+            case 3:
+                layoutView.image3.image = image
+                layoutView.image3.contentMode = .scaleAspectFill
+            case 4:
+                layoutView.image4.image = image
+                layoutView.image4.contentMode = .scaleAspectFill
+            default:
+                break
+            }
         }
         
         picker.dismiss(animated: true, completion: nil)
--- Instagrid/View/LayoutView.swift
+++ Instagrid/View/LayoutView.swift
@@ -7,36 +7,39 @@
 
 import UIKit
 
+//LayoutView class, contains the 3 or 4 imges of the grid
 class LayoutView: UIView {
     //MARK: -Properties
+    
+    //ImageView outlets for each image of the LayoutView
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
     @IBOutlet weak var image3: UIImageView!
     @IBOutlet weak var image4: UIImageView!
     
+    //ImageView outlets for each image of the layout selection
     @IBOutlet weak var selectedLayout1: UIImageView!
     @IBOutlet weak var selectedLayout2: UIImageView!
     @IBOutlet weak var selectedLayout3: UIImageView!
     
     //MARK: -Style
+    
+    //Enumerates the three possible layouts
     enum Style {
         case layout1, layout2, layout3
     }
     
+    //Calls setStyle when the attribute style is modified
     var style: Style = .layout3{
-        //Appelle la fonction setStyle lorsque la propriété style est modifiée
         didSet {
             setStyle(style)
         }
     }
     
+    //Switch containing every layout options, shows or hides the views needed
     private func setStyle (_ style: Style) {
         switch style {
         case.layout1:
-//            image1.image = #imageLiteral(resourceName: "Plus")
-//            image2.image = #imageLiteral(resourceName: "Plus")
-//            image3.image = #imageLiteral(resourceName: "Plus")
-//            image4.image = #imageLiteral(resourceName: "Plus")
             image1.isHidden = false
             image2.isHidden = true
             image3.isHidden = false
@@ -45,10 +48,6 @@ class LayoutView: UIView {
             selectedLayout2.isHidden = true
             selectedLayout3.isHidden = true
         case.layout2:
-//            image1.image = #imageLiteral(resourceName: "Plus")
-//            image2.image = #imageLiteral(resourceName: "Plus")
-//            image3.image = #imageLiteral(resourceName: "Plus")
-//            image4.image = #imageLiteral(resourceName: "Plus")
             image1.isHidden = false
             image2.isHidden = false
             image3.isHidden = false
@@ -57,10 +56,6 @@ class LayoutView: UIView {
             selectedLayout2.isHidden = false
             selectedLayout3.isHidden = true
         case.layout3:
-//            image1.image = #imageLiteral(resourceName: "Plus")
-//            image2.image = #imageLiteral(resourceName: "Plus")
-//            image3.image = #imageLiteral(resourceName: "Plus")
-//            image4.image = #imageLiteral(resourceName: "Plus")
             image1.isHidden = false
             image2.isHidden = false
             image3.isHidden = false
@@ -68,29 +63,6 @@ class LayoutView: UIView {
             selectedLayout1.isHidden = true
             selectedLayout2.isHidden = true
             selectedLayout3.isHidden = false
-        }
-    }
-    //MARK: -Selected Image
-    enum SelectedImage {
-        case image1, image2, image3, image4
-    }
-    
-    var selectedImage: SelectedImage = .image1{
-        didSet {
-            setSelection(selectedImage)
-        }
-    }
-    
-    private func setSelection (_ selectedImage: SelectedImage) {
-        switch selectedImage {
-        case .image1:
-            break
-        case .image2:
-            break
-        case .image3:
-            break
-        case .image4:
-            break
         }
     }
 }
